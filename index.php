cc75a3a931486c6693e26fb8761f04fe937f05be
Santi Dudok <434672@student.fontys.nl>
2019-11-01 13:48:01 +0100

    responsiveness

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php include("components/head.php"); ?>
-
 <?php include("components/header.php"); ?>
-
 <?php include("components/content.php"); ?>
-
 <?php include("components/download.php") ?>
-
 <?php include("components/video.php"); ?>
-
 <?php include("components/footer.php"); ?>
-
 <?php include("components/foot.php"); ?>
 
 
